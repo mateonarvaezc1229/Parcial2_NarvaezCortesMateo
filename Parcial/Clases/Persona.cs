5f7ae4b752e9cbd5a3ba2d924e333c09e822b272
Mateo Narvaez <mateonarvaez@correo.itm.edu.co>
2023-10-08 02:19:34 -0500

    Class Persona.cs created and personObject built in Program.cs

---
+++ Parcial/Clases/Persona.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial.Clases
+{
+    public class Persona
+    {
+        #region Fields
+
+        private int _year;
+        private int _month;
+        private int _day;
+
+        public Persona()
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        public Persona(int year, int month, int day)
+        {
+            _year = year;
+            _month = month;
+            _day = day; 
+
+                
+        }
+
+        public override string ToString()
+        { 
+            var dateConcatenated = _day + "/"+ _month + "/"+ _year; 
+            return dateConcatenated;
+        }
+
+
+
+        #endregion
+
+
+    }
+}
---
+++ Parcial/Program.cs
@@ -0,0 +1,34 @@
+﻿using Parcial.Clases;
+using System;
+namespace Parcial
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PARCIAL!");
+
+
+            int day, month, year;
+            Console.Write("Ingresar el dia :");
+            day = Convert.ToInt32(Console.ReadLine()); 
+
+
+            Console.Write("Ingresar el mes :");
+            month = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingresar el año :");
+            year = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Persona dateObject = new Persona(year,month,day);
+            Console.WriteLine(dateObject.ToString());
+
+        }
+    }
+}
+
+
+
